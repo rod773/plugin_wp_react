e42f962569a94d4d69bf8d8e4e71d573c9454450
rod7773 <rodrigoarcadio1@gmail.com>
2024-02-27 18:42:23 +0100

    update

---
+++ job-place.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:       WP React Kit
+ * Description:       A simple starter kit to work in WordPress plugin development using WordPress Rest API, WP-script and many more...
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Version:           0.8.0
+ * Tested upto:       6.2.2
+ * Author:            Maniruzzaman Akash<manirujjamanakash@gmail.com>
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       jobplace
+ */
+
+ 
